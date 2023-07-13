353d5109a3921f2d5dc7bd7030d207d8499b7bf5
Rafael Letnski Soares <rafael@gkult.net>
2023-07-13 11:15:42 -0300

    iniciando aula 04

---
+++ 02aula/js/02aula-b.js
@@ -0,0 +1,14 @@
+const a = 20
+const b = 15
+const sum = a + b
+console.log(sum)
+
+const a2 = 6
+const b2 = 3
+const div = a2 / b2
+console.log('A divisão é ' + div)
+
+const restDiv1 = 700
+const restDiv2 = 23
+const modulo = restDiv1 % restDiv2
+console.log("O modulo é: "+modulo)
---
+++ 02aula/js/02aula.js
@@ -0,0 +1,15 @@
+// Mostrando primeira mensagem
+console.log("Meu nome é Rafael")
+
+/*
+ 2a mensagem, e este tipo de comentário
+ é de várias linhas 
+ */
+console.log('Meu irmão se chama Ryan')
+
+// 3a mensagem
+console.log(23-15)
+
+// 4a mensagem
+let idade = 17
+console.log("Eu tenho "+ idade +" anos")
