c608cd79047341f62fa04791b6dc6f8fc3e09e14
Rafael L. Soares <rafael@gkult.net>
2023-07-19 20:04:23 -0300

    Meu primeiros programas em js

--- 06aula/js/main.js
+++ 06aula/js/main.js
@@ -5,7 +5,8 @@ Se menor que 6, reprovado
 Se maior ou igual a 6 e menor que 7, recuperação
 Se outros resultados, aprovado 
 */
-let nota = prompt('Qual foi sua média anual?')
+
+/*let nota = prompt('Qual foi sua média anual?')
 if (nota < 6) {
     console.log('Reprovado')
 }
@@ -15,3 +16,64 @@ else if (nota >= 6 && nota < 7) {
 else {
     console.log('Está Aprovado')
 }
+*/
+
+/*Exercício 1
+let idade = 25;
+console.log(idade)
+*/
+
+/*Exercício 2
+let x = 10
+let y = 5
+let soma = x + y
+console.log(soma)
+*/
+
+/*Exercício 3
+let nome = "Rafael"
+console.log("Saudações Rafael!")
+*/
+
+/*Exercício 4
+let temperatura = 25
+let resultado = temperatura > 30
+console.log(resultado)
+*/
+
+/*exercício 5
+let saldo = 1000
+console.log(saldo - 200)
+*/
+
+/*Exercício 6
+let altura = 10
+let largura = 5
+let áreaRetângulo = altura * largura
+console.log(áreaRetângulo)
+*/
+
+/*Exercício 7
+let login = "admin"
+let resultado = login === "admin"
+console.log(resultado) 
+*/
+
+/*Exercício 8
+let ano = 2022
+let resultado = ano >= 2021
+console.log(resultado)
+*/
+
+/*Exercício 9
+let preço = 10.50
+preço *= 1.2
+console.log(preço)
+*/
+
+/*let nomeCompleto = ""
+let nome = "Rafael"
+let sobrenome = "Soares"
+nomeCompleto = nome + " " + sobrenome
+console.log(nomeCompleto)
+*/
