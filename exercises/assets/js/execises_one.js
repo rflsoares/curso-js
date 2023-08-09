23a02f3e30d2a264c673dbc556055a5a31d6c228
Rogério Soares <rgrsoares@yahoo.com.br>
2023-08-09 11:29:14 -0300

    Create two lists exercices fundamental concepts JS

---
+++ exercises/assets/js/execises_one.js
@@ -0,0 +1,82 @@
+/* Exercício 1:
+Declare duas variáveis usando let e atribua a elas dois números inteiros. Em seguida, exiba a soma das variáveis usando console.log.
+
+*/ 
+
+/* Exercício 2:
+Declare uma variável constante com seu nome e exiba-a em um alerta usando alert.
+
+*/ 
+/* Exercício 3:
+Peça ao usuário para inserir um número usando prompt, multiplique esse número por 5 e exiba o resultado usando console.log.
+
+*/ 
+/* Exercício 4:
+Declare três variáveis usando let e atribua a elas as palavras "maçã", "banana" e "laranja". Concatene essas palavras em uma única string e exiba usando document.write.
+
+*/ 
+/* Exercício 5:
+Declare uma variável constante com um número qualquer. Peça ao usuário para inserir outro número usando prompt. Some os dois números e exiba o resultado em um alerta.
+
+*/ 
+/* Exercício 6:
+Declare uma variável constante e atribua a ela o valor booleano true. Em seguida, solicite ao usuário que confirme a veracidade da variável e exiba o resultado usando console.log.
+
+*/ 
+/* Exercício 7:
+Crie um programa que pergunte ao usuário seu nome usando prompt. Em seguida, use document.write para exibir uma saudação personalizada, concatenando o nome fornecido.
+
+*/ 
+/* Exercício 8:
+Declare duas variáveis com números inteiros diferentes. Compare essas variáveis usando operadores de comparação (>, <, >=, <=) e exiba os resultados usando console.log.
+
+*/ 
+/* Exercício 9:
+Peça ao usuário para inserir um número usando prompt. Verifique se o número é par ou ímpar e exiba o resultado em um alerta.
+
+*/ 
+/* Exercício 10:
+Declare uma constante com o valor de π (pi) e peça ao usuário para inserir o raio de um círculo usando prompt. Calcule e exiba a área desse círculo usando console.log.
+
+*/ 
+/* Exercício 11:
+Crie um programa que pergunte ao usuário se ele gosta de chocolate usando confirm. Exiba a resposta em um alerta.
+
+*/ 
+/* Exercício 12:
+Declare três variáveis usando let e atribua a elas números diferentes. Calcule a média desses números e exiba o resultado usando console.log.
+
+*/ 
+/* Exercício 13:
+Peça ao usuário para inserir seu ano de nascimento usando prompt. Calcule a idade do usuário e exiba em um alerta.
+
+*/ 
+/* Exercício 14:
+Declare uma constante contendo uma frase qualquer. Peça ao usuário para inserir uma palavra usando prompt. Verifique se a palavra está contida na frase usando includes e exiba o resultado em um alerta.
+
+*/ 
+/* Exercício 15:
+Crie um programa que pergunte ao usuário quantos anos ele tem usando prompt. Se a idade for maior ou igual a 18, exiba um alerta dizendo que a pessoa é maior de idade; caso contrário, exiba um alerta dizendo que é menor de idade.
+
+*/ 
+/* Exercício 16:
+Declare uma variável constante com o valor de uma temperatura em graus Celsius. Converta essa temperatura para Fahrenheit usando a fórmula F = (C * 9/5) + 32 e exiba o resultado usando console.log.
+
+*/ 
+/* Exercício 17:
+Peça ao usuário para inserir um número usando prompt. Verifique se o número é positivo, negativo ou zero, e exiba o resultado em um alerta.
+
+*/ 
+/* Exercício 18:
+Crie um programa que simule uma calculadora simples. Peça ao usuário para inserir dois números e a operação desejada (+, -, *, /). Realize a operação e exiba o resultado usando alert.
+
+*/ 
+/* Exercício 19:
+Declare três variáveis com nomes de cores. Peça ao usuário para escolher uma das cores usando prompt. Se a cor escolhida for uma das variáveis declaradas, exiba um alerta dizendo que essa cor está disponível; caso contrário, exiba um alerta dizendo que a cor não está disponível.
+
+*/ 
+/* Exercício 20:
+Crie um programa que pergunte ao usuário se ele quer jogar um jogo usando confirm. Se o usuário concordar, exiba um alerta de boas-vindas; caso contrário, exiba um alerta de despedida.
+
+Lembre-se de testar e verificar os resultados de cada exercício no navegador ou em um ambiente de desenvolvimento JavaScript.
+
---
+++ exercises/assets/js/execises_two.js
@@ -0,0 +1,97 @@
+
+/**Exercício 1:**
+Escreva um programa que verifica se um número é positivo, negativo ou zero e exibe o resultado.
+
+*/ 
+
+/**Exercício 2:**
+Crie um loop `for` que imprima os números de 1 a 10 no console.
+
+*/ 
+
+/**Exercício 3:**
+Escreva uma função que calcule o quadrado de um número e retorne o resultado.
+
+*/ 
+
+/**Exercício 4:**
+Crie um programa que determine se um número é par ou ímpar e exiba o resultado.
+
+*/ 
+
+/**Exercício 5:**
+Escreva uma função que receba dois números como parâmetros e retorne o maior deles.
+
+*/ 
+
+/**Exercício 6:**
+Crie um loop `for` que imprima os números pares de 2 a 20 no console.
+
+*/ 
+
+/**Exercício 7:**
+Escreva uma função que verifique se uma palavra é um palíndromo (lê-se igual da esquerda para a direita e vice-versa) e retorne `true` ou `false`.
+
+*/ 
+
+/**Exercício 8:**
+Crie um programa que calcule a média de um array de números.
+
+*/ 
+
+/**Exercício 9:**
+Escreva uma função que calcule o fatorial de um número.
+
+*/ 
+
+/**Exercício 10:**
+Crie um loop `for` que imprima a tabuada do 7 no console.
+
+*/ 
+
+/**Exercício 11:**
+Escreva uma função que receba um array de números e retorne a soma de todos os elementos.
+
+*/ 
+
+/**Exercício 12:**
+Crie um programa que determine se um ano é bissexto ou não.
+
+*/ 
+/**Exercício 13:**
+Escreva uma função que receba um número e retorne `true` se ele for primo, ou `false` caso contrário.
+
+*/ 
+
+/**Exercício 14:**
+Crie um loop `for` que imprima os primeiros 10 números da sequência de Fibonacci.
+
+*/ 
+
+/**Exercício 15:**
+Escreva uma função que converta uma temperatura de Celsius para Fahrenheit.
+
+*/ 
+
+/**Exercício 16:**
+Crie um programa que peça ao usuário para adivinhar um número entre 1 e 100. Informe se o palpite está acima, abaixo ou correto.
+
+*/ 
+
+/**Exercício 17:**
+Escreva uma função que remova os elementos duplicados de um array.
+
+*/ 
+
+/**Exercício 18:**
+Crie um programa que calcule o máximo divisor comum (MDC) entre dois números.
+
+*/ 
+
+/**Exercício 19:**
+Escreva uma função que calcule o valor da série de Fibonacci para um dado número `n`.
+
+*/ 
+
+/**Exercício 20:**
+Crie um programa que calcule e exiba os números primos entre 1 e 50.
