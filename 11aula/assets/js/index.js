042c4bce5f27a0b8253a7bf7ed8d716917f00e70
Rafael L. Soares <rafael@gkult.net>
2023-08-21 20:40:21 -0300

    Primeira aula de DOM

---
+++ 11aula/assets/js/index.js
@@ -0,0 +1,29 @@
+// let h1Titulo = document.querySelector("h1")
+// console.log(h1Titulo)
+
+// h1Titulo.textContent = "Alterei o título kkkkkk"
+
+// let altParag = document.querySelector("p")
+// console.log(altParag)
+
+// altParag.textContent = "E aí"
+
+// //Pegando elementos pelo id
+// let altParag2 = document.getElementById('segundo')
+// console.log(altParag2)
+// altParag2.textContent = "Bão?"
+
+// //Pegando elemento dos dois jeitos
+// let firstParag = document.querySelector('p')
+// let secondParag = document.querySelector('#segundo')
+
+// firstParag.textContent = 'alterei o primeiro'
+// secondParag.textContent = 'alterei o segundo'
+
+let primSpan = document.querySelector('span')
+console.log(primSpan)
+let secSpan = document.getElementById('destaque')
+primSpan.style.color = 'green'
+secSpan.style.fontSize = '20px'
+secSpan.style.fontWeight = '900'
+secSpan.style.borderBottom = 'solid 3px red'
